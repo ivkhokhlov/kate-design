2f093a5e7e649b1cf416b2d9c912c01612ad32b3
KateKhokhlova <kate.ionovaaa@gmail.com>
2026-06-05 16:09:07 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://ivkhokhlov.github.io",
+  site: "https://katekhokhlova.github.io",
   base: "/kate-design",
   devToolbar: {
     enabled: false
